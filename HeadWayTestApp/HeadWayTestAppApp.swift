584b36f56910f19f3c9264664da8989330f4e826
Bogdan Sarkisov <b.sarkisov@mobidev.biz>
2024-01-26 17:08:54 +0200

    Initial Commit

---
+++ HeadWayTestApp/HeadWayTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadWayTestAppApp.swift
+//  HeadWayTestApp
+//
+//  Created by User on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeadWayTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
